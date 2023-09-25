093ff18e5f147a71954629e5ca8dcd512908ce7c
Kaviyaravi23 <144332310+Kaviyaravi23@users.noreply.github.com>
2023-09-25 14:46:29 +0530

    Add files via upload

--- Challenge - Unit 3/linear_search_product.py
+++ Challenge - Unit 3/linear_search_product.py
@@ -1,1 +1,14 @@
+def linear_search_product(productlist,targetproduct):
+ indices=[]
+ for index,product in enumerate(productlist):
+   if product==targetproduct:
+    indices.append(index)
+   
+ return indices
+
+products=["shoes","boot","loafer","shoes","sandal","shoes"]
+target="shoes"
+target1="pen"
+result=linear_search_product(products,target)
+print(result)
 
